f2c97ee04d665ad00a26398cff048503db8370d2
sugarboo <kennethbaak@gmail.com>
2022-11-30 23:39:26 +0800

    feat: 00004-easy-pick

--- questions/00004-easy-pick/template.ts
+++ questions/00004-easy-pick/template.ts
@@ -1,1 +1,35 @@
-type MyPick<T, K> = any
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P]
+}
+
+/**
+ * js translation
+ */
+
+function myPick(todo, keys) {
+  const obj = {}
+
+  keys.forEach((key) => {
+    if (key in todo)
+      obj[key] = todo[key]
+  })
+
+  return obj
+}
+
+/**
+ * 1. Pick<Type, Keys>
+ * Reference: https://www.typescriptlang.org/docs/handbook/utility-types.html#picktype-keys
+ *
+ * 2. Indexed Access Types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+ *
+ * 3. Mapped Types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/mapped-types.html
+ *
+ * 4. Extends Types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/objects.html#extending-types
+ *
+ * 5. keyof Type Operator
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+ */
