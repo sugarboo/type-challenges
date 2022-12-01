3e3cd447c6e88db361cb8cc7c8ec79a28c1d23ae
kennethbai <bbkh@vip.qq.com>
2022-12-01 09:20:14 +0800

    feat: 00007-easy-readonly

--- questions/00007-easy-readonly/template.ts
+++ questions/00007-easy-readonly/template.ts
@@ -1,1 +1,17 @@
-type MyReadonly<T> = any
+type MyReadonly<T> = {
+  readonly [P in keyof T]: T[P]
+}
+
+/**
+ * 1. Readonly<Type>
+ * Reference: https://www.typescriptlang.org/docs/handbook/utility-types.html#readonlytype
+ *
+ * 2. readonly and const modifiers
+ * Reference: https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes-func.html#readonly-and-const
+ *
+ * 3. Indexed Access Types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+ *
+ * 4. keyof Type Operator
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/keyof-types.html
+ */
