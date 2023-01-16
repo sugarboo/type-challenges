0d6d248e58f6cecff7ada2bceb6459b1b8716875
kennethbai <bbkh@vip.qq.com>
2023-01-16 17:45:11 +0800

    feat: 00011-easy-tuple-to-object

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,1 +1,29 @@
-type TupleToObject<T extends readonly any[]> = any
+type TupleToObject<T extends readonly (string | number | symbol)[]> = {
+  [P in T[number]]: P
+}
+
+/**
+ * js translation
+ */
+
+function tupleToObject(array) {
+  // if key's type (elements in array) not string, number or symbol, throw error
+
+  const obj = {}
+
+  array.forEach((element) => {
+    obj[element] = element
+  })
+
+  return obj
+}
+
+/**
+ * 1. Conditional types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html
+ *
+ * > When Flatten is given an array type, it uses an indexed access with number to fetch out string[]’s element type. Otherwise, it just returns the type it was given.
+ *
+ * 2. @ts-check notations
+ * References: https://www.typescriptlang.org/docs/handbook/intro-to-js-ts.html#ts-check
+ */
