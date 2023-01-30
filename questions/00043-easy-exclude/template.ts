53acec163bc4f5d680ff979e1fb65cee4329d92e
kennethbai <bbkh@vip.qq.com>
2023-01-30 16:01:53 +0800

    feat: 00043-easy-exclude

--- questions/00043-easy-exclude/template.ts
+++ questions/00043-easy-exclude/template.ts
@@ -1,1 +1,9 @@
-type MyExclude<T, U> = any
+type MyExclude<T, U> = T extends U ? never : T
+
+/**
+ * 1. Exclude
+ * Reference: https://www.typescriptlang.org/docs/handbook/utility-types.html#excludeuniontype-excludedmembers
+ *
+ * 2. Distributive Conditional Types
+ * Reference: https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#distributive-conditional-types
+ */
