88b5004b6d4bf1038d7cd910ec38141015a1c233
kennethbai <bbkh@vip.qq.com>
2023-01-30 17:45:13 +0800

    feat: 00533-easy-concat

--- questions/00533-easy-concat/template.ts
+++ questions/00533-easy-concat/template.ts
@@ -1,1 +1,6 @@
-type Concat<T, U> = any
+type Concat<T extends unknown[], U extends unknown[]> = [...T, ...U]
+
+/**
+ * 1. Spread Operator
+ * Reference: https://www.typescriptlang.org/docs/handbook/variable-declarations.html#spread
+ */
