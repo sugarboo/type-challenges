05780368e443bc54f520dc3d7779a9cc3f9163e8
kennethbai <bbkh@vip.qq.com>
2023-01-30 17:28:04 +0800

    feat: 00268-easy-if

--- questions/00268-easy-if/template.ts
+++ questions/00268-easy-if/template.ts
@@ -1,1 +1,6 @@
-type If<C, T, F> = any
+type If<C extends Boolean, T, F> = C extends true ? T : F
+
+/**
+ * 1. Type Compatibility
+ * Reference: https://www.typescriptlang.org/docs/handbook/type-compatibility.html#handbook-content
+ */
