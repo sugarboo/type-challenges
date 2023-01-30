4754df044e3625ee50f118571cfe1303d14d3871
kennethbai <bbkh@vip.qq.com>
2023-01-30 17:14:01 +0800

    feat: 00189-easy-awaited

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,15 @@
-type MyAwaited<T> = any
+type MyAwaited<T extends PromiseLike<any>> = T extends PromiseLike<infer K>
+  ? K extends PromiseLike<any>
+    ? MyAwaited<K>
+    : K
+  : never
+
+/**
+ * 1. Awaited
+ * Reference: https://www.typescriptlang.org/docs/handbook/utility-types.html#awaitedtype
+ *
+ * 2. Promise
+ *
+ * 3. PromiseLike
+ * Reference: https://microsoft.github.io/PowerBI-JavaScript/interfaces/_node_modules_typedoc_node_modules_typescript_lib_lib_es5_d_.promiselike.html
+ */
