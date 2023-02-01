60a9d0872e323c09d1bd522428ccc7752c58f532
kennethbai <bbkh@vip.qq.com>
2023-02-01 17:11:43 +0800

    feat: 03312-easy-parameters

--- questions/03312-easy-parameters/template.ts
+++ questions/03312-easy-parameters/template.ts
@@ -1,1 +1,8 @@
-type MyParameters<T extends (...args: any[]) => any> = any
+type MyParameters<T extends (...args: any[]) => any> = T extends (...args: infer X) => any ? X : never
+
+/**
+ * 1. infer keyword
+ * Reference:
+ *  - https://www.typescriptlang.org/docs/handbook/2/conditional-types.html#inferring-within-conditional-types
+ *  - https://github.com/microsoft/TypeScript/pull/24897
+ */
